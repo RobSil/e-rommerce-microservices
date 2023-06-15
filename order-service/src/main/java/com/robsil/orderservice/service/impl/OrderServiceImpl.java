36155de38bd44c316bc4967c13eff0383358ed00
RobSil <djtim2014@gmail.com>
2023-06-16 00:01:55 +0300

    remove messages-client (some demo for oauth2, which doesn't uses in project), normalize settings.gradle

--- order-service/src/main/java/com/robsil/orderservice/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/robsil/orderservice/service/impl/OrderServiceImpl.java
@@ -67,6 +67,7 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
+    @Transactional
     public Order changeStatus(Long orderId, OrderStatus newStatus) {
         return changeStatus(findById(orderId), newStatus);
     }
